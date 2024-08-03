1556c3f7052e71240e43052947032745e328aa45
dilipd16 <diliprio14@gmail.com>
2024-08-03 11:31:11 +0530

    changes made in app and main

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,12 +1,13 @@
 import Youtube from "./Youtube"
+const username = "dilip"
 
 function App() {
   
 
   return (
     <>
-    <h1>Vite react app</h1>
-    <h1>Vite react app</h1>
+    <h1>Vite react app {2+2}</h1>
+    <h1>Vite react app {username}</h1>
     <Youtube />
     </>
   )
